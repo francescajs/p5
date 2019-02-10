0a551eabe635c926092dfccde5b2a2d4d189710d
Francesca Song <francescajsong@gmail.com>
2019-02-09 19:03:06 -0800

    draft 1

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,28 +1,37 @@
-var array = [0,0,0,0,0,0,0,0,0]
+var array = [2, 19, 24, 35, 40, 47, 52, 63, 77, 85, 91]
+var index = [0,1,2,3,4,5,6,7,8,9,10,11]
+var direction = "right";
+var act_count = 0;
+
+
 var dw = screen.width
+var backgroundColor;
+var isOverRight;
+var isOverLeft;
 
-var low_ind = 0
-var low_elem
-var high_elem
-var keyVal
+var bsize = 85
+var bspace = 60
+var numnum = 11
 
+var lowind = 0
+var highind = 10
+var midp = 5
+var keyVal = 52
 
-//function ascendingOrder() {
-  //points.sort(function(a, b){return a - b});
-//
-//Math.random()*(99)+1
+// let img;
+// function preload() {
+//   img = loadImage('/Users/francesca_song/Desktop/codebase/p5/empty-example/young.jpg');
+// }
 
 function setup() {
   // put setup code here
   // called once
-  dw = displayWidth
   createCanvas(displayWidth, displayHeight);
 
   noStroke();
   fill(250,175,200, 255);
   textSize(45);
   textAlign(CENTER);
-  textFont('Georgia');
   text("Binary Search", dw/2, 100);
   fill(110);
   rect(dw/2-135, 100+15, 270, 2);
@@ -30,30 +39,217 @@ function setup() {
   //Display target key
   fill(110);
   textSize(25);
-  text("Search for " + keyVal, dw/2, 100+80);
-
-  //populate array
-  var numbers = [1,2,3,4,5,6,7,8,9]
-
-  function populate(a) {
-  	for
-  }
-
-
+  text("Search for " + keyVal + "!", dw/2, 100+80);
+  
 }
 
 //let value = 0;
 
-function draw() {
-  // put drawing code here
-  //fill(value);
-  //ellipse(random(45, 600), random(45, 600), random(30,70), random(30,70));
+
+function drawOneRect(i) {
+  noStroke();
+  fill(250,175,200, 255);
+  var x = dw - 1128 + (3 / 2) * i * bspace;
+  rect(x , 270, bsize, bsize, bsize/4);
+  fill(90);
+  textAlign(LEFT);
+  textSize(15);
+  text(index[i], x + 1, 268);
+  textAlign(CENTER, CENTER);
+  textSize(35);
+  text(array[i], x + 43, 315);
 }
 
-function mouseClicked() {
-	//if (value === 0) {
-	//	value = color(random(255), random(255), random(255));
-	//} else {
-	//	value = 0;
-	//}
+function drawYoung(i) {
+  noStroke();
+  var x = dw - 1128 + (3 / 2) * i * bspace;
+  rect(x , 270, bsize, bsize, bsize/4);
+  fill(90);
+  textAlign(LEFT);
+  textSize(15);
+  text(index[i], x + 1, 268);
+  image(img, x, 270, bsize, bsize);
 }
+
+function shadeB(i) {
+  noStroke();
+  fill(90);
+  var x = dw - 1128 + (3 / 2) * i * bspace;
+  rect(x , 270, bsize, bsize, bsize/4);
+  textAlign(LEFT);
+  textSize(15);
+  text(index[i], x + 1, 268);
+}
+
+var SPACING = 20;
+var BUTTON_WIDTH = 180;
+var BUTTON_HEIGHT = 60;
+var R_BTN_X = dw/2 + SPACING;
+var L_BTN_X = dw/2 - BUTTON_WIDTH - SPACING;
+var BUTTON_Y = 500;
+
+
+function drawButtons() {
+  noStroke();
+ 
+  textSize(30);
+
+  rectMode(CORNER);
+
+  // Right 
+  //check if mouse is in side the rect.
+  // mouseX >= x && mouseX <= x+BUTTON_WIDTH && mouseY >= BUTTON_Y && mouseY <= BUTTON_Y + BUTTON_HEIGHT 
+  if (mouseX >= R_BTN_X && mouseX <= R_BTN_X + BUTTON_WIDTH && mouseY >= BUTTON_Y && mouseY <= BUTTON_Y + BUTTON_HEIGHT) {
+   // mouseX <= R_BTN_X + BUTTON_WIDTH) {
+    isOverRight = true;
+  } else {
+    isOverRight = false;
+  }
+  
+  if (isOverRight == true) {
+    //console.log("isOverButton T")
+    fill(150);
+    cursor(HAND);
+  } else {
+    //console.log("isOverButton is F")
+    fill(90);
+    cursor(ARROW);
+  }
+  rect(R_BTN_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT, 10);
+
+  fill(255);
+  textAlign(CENTER, CENTER);
+  text("Go RIGHT", dw/2 + SPACING + BUTTON_WIDTH/2, BUTTON_Y+BUTTON_HEIGHT/2);
+
+  // Left
+  //check if mouse is in side the rect.
+  // mouseX >= x && mouseX <= x+BUTTON_WIDTH && mouseY >= BUTTON_Y && mouseY <= BUTTON_Y + BUTTON_HEIGHT 
+  if (mouseX >= L_BTN_X && mouseX <= L_BTN_X + BUTTON_WIDTH && mouseY >= BUTTON_Y && mouseY <= BUTTON_Y + BUTTON_HEIGHT) {
+   // mouseX <= R_BTN_X + BUTTON_WIDTH) {
+    isOverLeft = true;
+  } else {
+    isOverLeft = false;
+  }
+
+  if (isOverLeft == true) {
+    //console.log("isOverButton is T")
+    fill(150);
+    cursor(HAND);
+  } else {
+    //console.log("isOverButton is F")
+    fill(90);
+    cursor(ARROW);
+  }
+  
+  rect(L_BTN_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT, 10);
+
+  fill(255);
+  textAlign(CENTER, CENTER);
+  text("Go LEFT", dw/2 - SPACING - BUTTON_WIDTH/2, BUTTON_Y+BUTTON_HEIGHT/2);
+
+  //annotation below buttons
+  fill(160);
+  textSize(20);
+  text("Discard Right", dw/2 - SPACING - BUTTON_WIDTH/2, BUTTON_Y + 90);
+  text("Discard Left", dw/2 + SPACING + BUTTON_WIDTH/2, BUTTON_Y + 90);
+}
+
+
+function mousePressed() {
+
+  if (isOverLeft) {
+    //clicked left
+    console.log("left")
+    if (direction == "left") {
+      act_count += 1;
+    } else {
+      alert("Try again!");
+    }
+  }
+
+  if (isOverRight) {
+    console.log("right")
+    if (direction == "right"){
+      act_count += 1;
+    } else {
+      alert("Try again!");
+    }
+  }
+}
+
+// order of the actual binary process
+  //first action (mid = 47, highlight ind 6 --> 10)
+function firstAct(i) {
+
+  console.log("firstAct");
+  if (i <= 5) {
+    //drawYoung(i);
+    shadeB(i);
+  } else {
+    drawOneRect(i);
+  }
+
+  // lowind = 6;
+  // highind = 10;
+  // midp = 8;
+  direction = "left";
+}
+
+  //second action (mid = 77, highlight ind 6 --> 7)
+function secAct(i) {
+
+  if (i <= 5 || i >= 8 ) {
+    //drawYoung(i);
+    shadeB(i);
+  } else {
+    drawOneRect(i);
+  }
+
+//   lowind = 6;
+//   highind = 7;
+//   midp = 6;
+}
+
+//last action (mid = 52, highligh '52' -- found!)
+function lastAct(i) {
+
+  if (i != 6){
+    //drawYoung(i);
+    shadeB(i);
+  } else {
+    drawOneRect(i);
+  }
+  dOne();
+}
+
+function dOne() {
+  fill(250,175,200, 255);
+  textSize(30);
+  textAlign(CENTER);
+  text("You found " + keyVal + "!", dw/2, 660);
+}
+
+function draw() {
+  drawButtons()
+  var i;
+  for (i = 0; i < array.length; i++) {
+    if (act_count == 0) {
+      drawOneRect(i);
+    }
+
+    if (act_count == 1) {
+      firstAct(i);
+    }
+
+    if (act_count == 2) {
+      secAct(i);
+     
+    }
+
+    if (act_count == 3) {
+      lastAct(i);
+    }
+  }
+}
+
+  
